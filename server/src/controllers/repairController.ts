e237e23e0eb0397148add01c982f7736b8f81be7
Matt Kelsey <mattkelseymk@gmail.com>
2018-12-27 20:04:25 -0800

    Basic create and get repairs implementation

--- server/src/controllers/repairController.ts
+++ server/src/controllers/repairController.ts
@@ -2,16 +2,16 @@ import * as mongoose from 'mongoose';
 import { RepairSchema } from '../models/repairModel';
 import { Request, Response } from 'express';
 
-const Repair = mongoose.model('Contact', RepairSchema);
-export class ContactController{
+const Repair = mongoose.model('Repair', RepairSchema);
+export class RepairController {
   public newRepair (req: Request, res: Response) {                
     let newRepair = new Repair(req.body);
     
-      newRepair.save((err, contact) => {
-        if(err){
-          res.send(err);
-        }    
-        res.json(contact);
-      });
-    }
+    newRepair.save((err, repair) => {
+      if(err){
+        res.send(err);
+      }
+      res.json(repair);
+    });
   }
+}
--- server/src/routes/Repairs.ts
+++ server/src/routes/Repairs.ts
@@ -1,10 +1,19 @@
+import { RepairController } from "../controllers/repairController";
+import { Request, Response } from "express";
+import * as mongoose from 'mongoose';
+import { RepairSchema } from "../models/repairModel"
 
-import {Request, Response} from "express";
+const Repair = mongoose.model('Repair', RepairSchema);
 export class Repairs { 
+    public repairController: RepairController = new RepairController();
     public routes(app): void { //received the express instance from app.ts file         
         app.route('/repair/:repairId')
-        .get((req: Request, res: Response) => {            
-            res.status(200).send("yeet");
-        })
+          .get((req: Request, res: Response) => {            
+              Repair.findOne({ 'repairId': req.params.repairId }, (err, repair) => {
+                res.send(repair);
+              })
+          })
+
+        app.route('/repair/').post(this.repairController.newRepair)
     }
 }
