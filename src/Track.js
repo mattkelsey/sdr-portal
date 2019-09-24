d4f73ef0b750285e1274a18618ecbf06586b416e
Kale Zweig <KaleZweig@users.noreply.github.com>
2019-09-23 18:33:21 -0700

    Update Track.js

--- src/Track.js
+++ src/Track.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 class Track extends React.Component{
@@ -8,6 +9,10 @@ class Track extends React.Component{
       <div className="page">
         <h1>Repair Status</h1>
         <p>
+
+          <h3>Repair ID</h3>
+          <div>#kal1417</div>
+
           <h3>Contact Info</h3>
           <div>Kale Z</div>
           <div>kale@seattledronerepair.com</div>
@@ -15,21 +20,24 @@ class Track extends React.Component{
 
           <h3>Drop off</h3>
           <div>Method Ship in</div>
+          <Link to='/'>Print Shipping Label</Link>
           <div>Tracking 38828429939203020</div>
           <div>Recieved False</div>
 
+          <h3>Drop off</h3>
+          <div>Method Drop off</div>
+          <div>Recieved False</div>
+
           <h3>Diagnostics</h3>
-          <div>Done True</div>
           <div>Technician Notes: Drone had damged ribbon</div>
           <div>Estimated Cost $143</div>
-          <button className="submitTkt">Approve</button>
-
-          <h3>Paperwork</h3>
           <div>Completed False</div>
-          <a>Complete Now</a>
+          <Link to='/signature'>Sign Paperwork</Link>
 
           <h3>Status</h3>
           <div>Awaiting Parts</div>
+          <div>Picked up false</div>
+          <div>Return Tracking</div>
 
         </p>
 
